ce181f9bcff28594957ad4922ca3197cf9a7652c
Nathan Tofte <nathan.tofte@gmail.com>
2025-03-17 19:28:44 -0500

    updated constants for wrist

--- src/main/java/frc/robot/constants/WristConstants.java
+++ src/main/java/frc/robot/constants/WristConstants.java
@@ -1,10 +1,10 @@
 package frc.robot.constants;
 
 public final class WristConstants {
-    public static final double L4_Angle = 0.5;
-    public static final double L3_Angle = 0.45;
-    public static final double Source_Angle = 0.28;
-    public static final double Rest_Angle = 0.188;
+    public static final double L4_Angle =  0.68; //  0.5;
+    public static final double L3_Angle = 0.72; // 0.45;
+    public static final double Source_Angle = 0.88; // 0.28;
+    public static final double Rest_Angle = 0.98; // 0.188;
     public static final double Forward_Angle = 0.59;
     public static final double Alage_Angle = 0.52
     ;
